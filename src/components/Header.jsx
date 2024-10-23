739071d8ed99eb8f869aa06f1772af2d63c42c3e
YunDoll <mar0722@naver.com>
2024-10-23 22:16:26 +0900

    chore: 프로젝트 기본 구조 설정

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 150px;
+  background-color: #a1131f;
+`;
+
+const Header = () => {
+  return <HeaderContainer></HeaderContainer>;
+};
+
+export default Header;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+
+const MainContainer = styled.div`
+  min-height: 100vh;
+  background-color: #2c2c2c;
+`;
+
+const Main = () => {
+  return (
+    <MainContainer>
+      <Header></Header>
+    </MainContainer>
+  );
+};
+
+export default Main;
