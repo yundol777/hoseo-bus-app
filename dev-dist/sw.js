2d1309825dd0272167152831b6461117208182e3
yundoll <mar0722@naver.com>
2025-11-18 15:27:13 +0900

    chore: 개발 환경 업데이트

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-8403d43d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oqdhi8u812o"
+    "revision": "0.o4vnc1se0cg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
